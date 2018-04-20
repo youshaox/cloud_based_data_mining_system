13ec97b682d9f4bf29e1c9bf77b28de1381820e1
youshaoxiao <youshao.xiao@shuyun.com>
2018-04-20 19:03:18 +1000

    first commit

---
+++ 3_crawler/twitter_streaming_crawler.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# Created by youshaox on 20/4/18
+"""
+function:
+
+"""
+import sys
+
+#解决 二进制str 转 unicode问题
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        pass
+    except Exception as e:
+        print(e)
