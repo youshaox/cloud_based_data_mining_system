d8025cdcd73af2faa006a8881c4498bca8cba81e
youshaoxiao <youshao.xiao@shuyun.com>
2018-04-30 19:50:35 +1000

    deploy + controller finished 改volume 因为pt不带volume

--- 2_couchdb/setup.sh
+++ 2_couchdb/setup.sh
@@ -51,12 +51,10 @@ nohup sh /data/workspace/apache-couchdb-2.1.1/rel/couchdb/bin/couchdb 2>&1 &
 # 4. 节点
 
 curl -X POST -H "Content-Type: application/json" http://admin:admin@127.0.0.1:5984/_cluster_setup -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "admin", "password":"admin", "node_count":"3"}'
-
 # todo 改remote ip
 curl -X POST -H "Content-Type: application/json" http://admin:admin@127.0.0.1:5984/_cluster_setup -d '{"action": "enable_cluster", "bind_address":"0.0.0.0", "username": "admin", "password":"admin", "port": 5984, "node_count": "3", "remote_node": "115.146.85.226", "remote_current_user": "admin", "remote_current_password": "admin" }'
-
+# todo 改remote ip
 curl -X POST -H "Content-Type: application/json" http://admin:admin@127.0.0.1:5984/_cluster_setup -d '{"action": "add_node", "host":"115.146.85.226", "port": "5984", "username": "admin", "password":"admin"}'
-
 curl -X POST -H "Content-Type: application/json" http://admin:admin@127.0.0.1:5984/_cluster_setup -d '{"action": "finish_cluster"}'
 
 ## adding nodes 改localhost 和 name和 remote ip
