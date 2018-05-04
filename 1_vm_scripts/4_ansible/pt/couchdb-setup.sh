2fd9c83efec31bb674e606c2aaa372ebd5bae0ca
youshaoxiao <youshao.xiao@shuyun.com>
2018-05-04 15:12:48 +1000

    ansible pt first_version

--- 1_vm_scripts/4_ansible/pt/couchdb-setup.sh
+++ 1_vm_scripts/4_ansible/pt/couchdb-setup.sh
@@ -3,10 +3,12 @@ cd /data/workspace
 wget http://apache.mirror.amaze.com.au/couchdb/source/2.1.1/apache-couchdb-2.1.1.tar.gz
 tar -zxvf apache-couchdb-2.1.1.tar.gz
 cd apache-couchdb-2.1.1/
+sudo rm -rf /usr/lib/erlang/man
 ./configure && make release
 
 # configure
-cd /data/workspace/apache-couchdb-2.1.1/rel/couchdb/etc;sed -i "s/;admin = mysecretpassword/admin = admin/g" local.ini
+cd /data/workspace/apache-couchdb-2.1.1/rel/couchdb/etc
+sed -i "s/;admin = mysecretpassword/admin = admin/g" local.ini
 sed -i 's/127.0.0.1/0.0.0.0/g' default.ini
 
 cd /data/workspace/apache-couchdb-2.1.1/rel/couchdb/releases/2.1.1;rm -rf sys.config
