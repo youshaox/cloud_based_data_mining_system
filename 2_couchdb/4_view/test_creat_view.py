3145a51218e97f7dddf6a242727e59790053fd82
linaz3 <38577812+linaz3@users.noreply.github.com>
2018-05-10 02:44:16 +1000

    Update test_creat_view.py

--- 2_couchdb/4_view/test_creat_view.py
+++ 2_couchdb/4_view/test_creat_view.py
@@ -1,7 +1,14 @@
+#---group 25----
+# Youshao Xiao - 876548
+# Jiaheng Zhu - 848432
+# Lina Zhou - 941539
+# Haimei Liu - 895804
+# Miaomiao Zhang - 895216
+
 import couchdb
 import create_View
 
-sever_instance = couchdb.Server('http://admin:admin@115.146.86.138:5984/')
+sever_instance = couchdb.Server('http://admin:admin@XXXX.XXXX.XX.XXX:5984/')
 
 # create_view (couch, db, map_name, reduce_name)
 # get_view(couch, db, design_view_name, g_level=1):
@@ -10,9 +17,8 @@ sever_instance = couchdb.Server('http://admin:admin@115.146.86.138:5984/')
 # group_level = 1 for the number of tweets on each state
 # group_level = 2 for the number of tweets on each district in Victoria
 # group_level = 3 for the number of tweets on each district in Melbourne
-create_View.create_view(sever_instance, "processed_data_new_system", "district", "_count")
-view = create_View.get_view(sever_instance, "processed_data_new_system", "district/district", g_level=1)
-
+create_View.create_view(sever_instance, "tagged_tweet", "district", "_count")
+view = create_View.get_view(sever_instance, "tagged_tweet", "district/district", g_level=1)
 for row in view:
     print(row)
 
@@ -20,28 +26,59 @@ for row in view:
 # group_level = 1 for the accumulated value of each state
 # group_level = 2 for the accumulated value of each district in Victoria
 # group_level = 3 for the accumulated value of each district in Melbourne
-create_View.create_view(sever_instance, "processed_data_new_system", "sentiment", "_sum")
-view = create_View.get_view(sever_instance, "processed_data_new_system", "sentiment/sentiment", g_level=1)
+create_View.create_view(sever_instance, "tagged_tweet", "sentiment", "_sum")
+view = create_View.get_view(sever_instance, "tagged_tweet", "sentiment/sentiment", g_level=1)
 for row in view:
     print(row)
 
 
 #group_level = 2 for the number of each system on each state
-create_View.create_view(sever_instance, "processed_data_new_system", "system", "_count")
-view = create_View.get_view(sever_instance, "processed_data_new_system", "system/system", g_level=2)
+create_View.create_view(sever_instance, "tagged_tweet", "system", "_count")
+view = create_View.get_view(sever_instance, "tagged_tweet", "system/system", g_level=2)
+for row in view:
+    print(row)
+
+print("--------------------------------------------------- emoji_states")
+#group_level = 2 for counting the times of each expression on each state
+create_View.create_view(sever_instance, "tagged_tweet", "emoji_States", "_count")
+view = create_View.get_view(sever_instance, "tagged_tweet", "emoji_States/emoji_States", g_level=2)
 for row in view:
     print(row)
 
 
-create_View.create_view(sever_instance, "raw_tweets", "process_raw", None)
-view = create_View.get_view(sever_instance, "raw_tweets", "process_raw/process_raw",None)
+print("--------------------------------------------------- emoji_Viatoria")
+#group_level = 2 for counting the times of each expression on each district in Viatoria
+create_View.create_view(sever_instance, "tagged_tweet", "emoji_Vic", "_count")
+view = create_View.get_view(sever_instance, "tagged_tweet", "emoji_Vic/emoji_Vic", g_level=2)
+for row in view:
+    print(row)
+
+print("--------------------------------------------------- emoji_Melbourne")
+#group_level = 2 for counting the times of each expression on each district in Melbourne
+create_View.create_view(sever_instance, "ptagged_tweet", "emoji_Mel", "_count")
+view = create_View.get_view(sever_instance, "tagged_tweet", "emoji_Mel/emoji_Mel", g_level=2)
+for row in view:
+    print(row)
+
+
+
+print("--------------------------------------------------- raw_data")
+
+create_View.create_view(sever_instance, "raw_tweet", "process_raw", None)
+view = create_View.get_view(sever_instance, "raw_tweet", "process_raw/process_raw",None)
 for row in view:
     print(row)
     break
 
-
-create_View.create_view(sever_instance, "aurin", "get_doc", None)
-view = create_View.get_view(sever_instance, "aurin", "get_doc/get_doc",None)
+print("--------------------------------------------------- aurin")
+create_View.create_view(sever_instance, "aurin_data", "get_doc", None)
+view = create_View.get_view(sever_instance, "aurin_data", "get_doc/get_doc",None)
 for row in view:
     print(row)
 
+
+print("--------------------------------------------------- result")
+create_View.create_view(sever_instance, "final_result", "get_doc", None)
+view = create_View.get_view(sever_instance, "final_result", "get_doc/get_doc",None)
+for row in view:
+    print(row)
