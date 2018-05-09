655b9b83ea8e35a8b6e2338c63789bf81bda97f6
youshaoxiao <youshao.xiao@shuyun.com>
2018-05-10 09:06:59 +1000

    github-public; security; KEY-PAIR REMOVED todo 1. 整理各个文件夹

--- 1_vm_scripts/backup/3_crawler/object_storage.py
+++ 1_vm_scripts/backup/3_crawler/object_storage.py
@@ -1,8 +1,8 @@
 from boto.ec2.regioninfo import RegionInfo
 import boto
 
-s3_conn = boto.connect_s3(aws_access_key_id='4fe68d160f60423bb0ff819f28f162f8',
-                          aws_secret_access_key='3e153f93268043b3b1717825921ff706',
+s3_conn = boto.connect_s3(aws_access_key_id='REMOVED',
+                          aws_secret_access_key='REMOVED',
                           is_secure=True,
                           host='swift.rc.nectar.org.au',
                           port=8888,
