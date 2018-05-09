b6628feeb60e59e439a06823bee60067795a67c6
linaz3 <38577812+linaz3@users.noreply.github.com>
2018-05-10 02:57:23 +1000

    Update system.js

--- 2_couchdb/4_view/map_functions/system.js
+++ 2_couchdb/4_view/map_functions/system.js
@@ -1,3 +1,10 @@
+//group 25
+//* Youshao Xiao - 876548
+//* Jiaheng Zhu - 848432
+//* Lina Zhou - 941539
+//* Haimei Liu - 895804
+//* Miaomiao Zhang - 895216
+
 //Map function to find the the distribution of phone system on each state
 // _count as reduce function
 //group_level = 2 for the number of each system on each state
